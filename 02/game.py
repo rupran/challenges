5cbf58499c50095599f64f5583d3cce0398f4b61
Andreas Ziegler <andreas.ziegler@fau.de>
2017-01-18 10:34:09 +0100

    Day 2: make it testable

--- 02/game.py
+++ 02/game.py
@@ -1,4 +1,4 @@
-#!python3
+#!/usr/bin/env python3
 # Code Challenge 02 - Word Values Part II - a simple game
 # http://pybit.es/codechallenge02.html
 
@@ -21,30 +21,47 @@ def max_word_value(words):
     """Calc the max value of a collection of words"""
     return max(words, key=calc_word_value)
 
+def draw_letters():
+    """Draw NUM_LETTERS letters randomly from POUCH"""
+    return random.sample(POUCH, NUM_LETTERS)
 
-def main():
-    # Draw letters
-    cur_letters = random.sample(POUCH, NUM_LETTERS)
-    print("Letters drawn: {}".format(str(", ".join(cur_letters))))
-
-    # Get user input, validate and score
+def get_user_input(draw):
+    """Read the user input and validate it"""
     user_word = input("Form a valid word: ")
     if user_word.lower() not in DICTIONARY:
         print("Unknown word: {}".format(user_word))
         sys.exit(1)
 
-    if any(x not in cur_letters for x in user_word.upper()):
+    if any(x not in draw for x in user_word.upper()):
         print("Can not build {} from letters".format(user_word))
         sys.exit(1)
 
+    return user_word
+
+def get_possible_dict_words(draw):
+    """Calculate possible words by generating all permutations from draw and
+    checking if the words are in DICTIONARY"""
+    return [word for word in _get_permutations_draw(draw)
+            if word.lower() in DICTIONARY]
+
+def _get_permutations_draw(draw):
+    """Generate all permutations with 1 to NUM_LETTERS letters from draw"""
+    return ("".join(word) for i in range(1, NUM_LETTERS + 1)
+            for word in itertools.permutations(draw, i))
+
+def main():
+    """Main function"""
+    # Draw letters
+    cur_letters = draw_letters()
+    print("Letters drawn: {}".format(str(", ".join(cur_letters))))
+
+    # Get user input, validate and score
+    user_word = get_user_input(cur_letters)
     user_score = calc_word_value(user_word)
     print("Word chosen: {} (value: {})".format(user_word.upper(), user_score))
 
-    # Calculate possible words by generating all permutations with 1 to
-    # NUM_LETTERS letters and checking if the word is in the dictionary
-    possible_words = ["".join(word) for i in range(1, NUM_LETTERS + 1)
-                      for word in itertools.permutations(cur_letters, i)
-                      if "".join(word).lower() in DICTIONARY]
+    # Get possible words
+    possible_words = get_possible_dict_words(cur_letters)
 
     # Get optimal word, its score and calculate score
     optimal_word = max_word_value(possible_words)
